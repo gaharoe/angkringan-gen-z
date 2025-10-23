977c2066c514b9c77215b3c9dca6324a50eda52e
Ilham Candra Mukti <153198261+gaharoe@users.noreply.github.com>
2025-10-23 19:07:51 +0700

    Update installer.cmd

--- backend/installer.cmd
+++ backend/installer.cmd
@@ -1,4 +1,4 @@
-winget install OpenJS.NodeJS Git.Git --accept-source-agreement
+winget install OpenJS.NodeJS Git.Git --accept-source-agreements
 cd C:\Users\%USERNAME%\Documents
 git clone https://github.com/gaharoe/angkringan-gen-z
 cd angkringan-gen-z
